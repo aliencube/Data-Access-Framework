86573b7748f42367f62d8c4b3609c45c0044b7f1
Justin Yoo <justin.yoo@aliencube.com>
2013-09-20 14:30:38 +1000

    Added sample entities.

---
+++ SourceCodes/DataAccessFramework/ApplicationDataContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+
+namespace DataAccessFramework
+{
+	/// <summary>
+	/// This represents the application data context entity.
+	/// </summary>
+	public partial class ApplicationDataContext : DbContext
+	{
+		/// <summary>
+		/// Gets or sets the list of users.
+		/// </summary>
+		public DbSet<User> Users { get; set; }
+
+		/// <summary>
+		/// Gets or sets the list of members.
+		/// </summary>
+		public DbSet<Member> Members { get; set; }
+	}
+}
