d0c4246b809dcef3a1e87562200f6561a9177aa0
Justin Yoo <justin.yoo@aliencube.com>
2013-09-23 16:33:48 +1000

    Added type configuration for fluent API.

---
+++ SourceCodes/DataAccessFramework/MemberTypeConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+
+namespace DataAccessFramework
+{
+    /// <summary>
+    /// This represents a member type configuration entity.
+    /// </summary>
+    public class MemberTypeConfiguration : EntityTypeConfiguration<Member>
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initialises a new instance of the MemberTypeConfiguration object.
+        /// </summary>
+        public MemberTypeConfiguration()
+        {
+            this.HasKey(p => p.MemberId);
+            this.Property(p => p.MemberId).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            this.Property(p => p.GivenNames).IsRequired().HasMaxLength(128);
+            this.Property(p => p.Surname).IsRequired().HasMaxLength(128);
+            this.Property(p => p.DateCreated).IsRequired();
+            this.Property(p => p.CreatedBy).IsRequired();
+
+            this.HasRequired(p => p.User).WithMany(p => p.Members).HasForeignKey(p => p.UserId).WillCascadeOnDelete();
+        }
+
+        #endregion Constructors
+    }
+}
---
+++ SourceCodes/DataAccessFramework/UserTypeConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+
+namespace DataAccessFramework
+{
+    /// <summary>
+    /// This represents a user type configuration entity.
+    /// </summary>
+    public class UserTypeConfiguration : EntityTypeConfiguration<User>
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initialises a new instance of the UserTypeConfiguration object.
+        /// </summary>
+        public UserTypeConfiguration()
+        {
+            this.HasKey(p => p.UserId);
+            this.Property(p => p.UserId).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            this.Property(p => p.Username).IsRequired().HasMaxLength(32);
+            this.Property(p => p.Password).IsRequired().HasMaxLength(64);
+            this.Property(p => p.Email).IsRequired().HasMaxLength(128);
+            this.Property(p => p.DateCreated).IsRequired();
+            this.Property(p => p.CreatedBy).IsRequired();
+        }
+
+        #endregion Constructors
+    }
+}
