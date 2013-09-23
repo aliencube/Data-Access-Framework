b226db8123d823393b498ce066e618109a9eb992
Justin Yoo <justin.yoo@aliencube.com>
2013-09-23 10:15:45 +1000

    Added more properties to the connection details element.

--- SourceCodes/DataAccessFramework.Configuration/ConnectionDetailsElement.cs
+++ SourceCodes/DataAccessFramework.Configuration/ConnectionDetailsElement.cs
@@ -8,6 +8,8 @@ namespace DataAccessFramework.Configuration
 	/// </summary>
 	public class ConnectionDetailsElement : ConfigurationElement
 	{
+		#region General
+
 		/// <summary>
 		/// Gets or sets the database context name to be used in Entity Framework.
 		/// Default value is <c>ApplicationDataContext</c>.
@@ -64,6 +66,72 @@ namespace DataAccessFramework.Configuration
 			set { this["use"] = value; }
 		}
 
+		#endregion General
+
+		#region	Advanced
+
+		/// <summary>
+		/// Gets or sets a value that indicates whether multiple active result sets
+		/// can be associated with the associated connection.
+		/// Default value is <c>False</c>.
+		/// </summary>
+		[ConfigurationProperty("multipleActiveResultSets", DefaultValue = false, IsRequired = false)]
+		public bool MultipleActiveResultSets
+		{
+			get { return (bool)this["multipleActiveResultSets"]; }
+			set { this["multipleActiveResultSets"] = value; }
+		}
+
+		/// <summary>
+		/// Gets or sets a string that contains the name of the network library
+		/// used to establish a connection to the SQL Server.
+		/// Default value is <see cref="String.Empty" />.
+		/// </summary>
+		[ConfigurationProperty("networkLibrary", DefaultValue = "", IsRequired = false)]
+		public string NetworkLibrary
+		{
+			get { return (string)this["networkLibrary"]; }
+			set { this["networkLibrary"] = value; }
+		}
+
+		/// <summary>
+		/// Gets or sets the size in bytes of the network packets used to communicate with an instance of SQL Server.
+		/// Default value is <c>8000</c>.
+		/// </summary>
+		[ConfigurationProperty("packetSize", DefaultValue = 8000, IsRequired = false)]
+		public int PacketSize
+		{
+			get { return (int)this["packetSize"]; }
+			set { this["packetSize"] = value; }
+		}
+
+		/// <summary>
+		/// Gets or sets a string value that indicates how the connection maintains its
+		/// association with an enlisted <see cref="System.Transactions"/> transaction.
+		/// Default value is <c>Implicit Unbind</c>.
+		/// </summary>
+		[ConfigurationProperty("transactionBinding", DefaultValue = "Implicit Unbind", IsRequired = false)]
+		public string TransactionBinding
+		{
+			get { return (string)this["transactionBinding"]; }
+			set { this["transactionBinding"] = value; }
+		}
+
+		/// <summary>
+		/// Gets or sets a string value that indicates the type system the application expects.
+		/// Default value is <c>Latest</c>.
+		/// </summary>
+		[ConfigurationProperty("typeSystemVersion", DefaultValue = "Latest", IsRequired = false)]
+		public string TypeSystemVersion
+		{
+			get { return (string)this["typeSystemVersion"]; }
+			set { this["typeSystemVersion"] = value; }
+		}
+
+		#endregion
+
+		#region	Context
+
 		/// <summary>
 		/// Gets or sets the name of the application associated with the connection string.
 		/// Default value is <c>.NET SqlClient Data Provider</c>.
@@ -76,6 +144,37 @@ namespace DataAccessFramework.Configuration
 		}
 
 		/// <summary>
+		/// Gets or sets the name of the workstation connecting to SQL Server.
+		/// Default value is <see cref="String.Empty" />.
+		/// </summary>
+		/// <remarks>If <c>IntegratedSecurity</c> is set to <c>True</c>, this is not necessary.</remarks>
+		[ConfigurationProperty("workstationId", DefaultValue = "", IsRequired = false)]
+		public string WorkstationId
+		{
+			get { return (string)this["workstationId"]; }
+			set { this["workstationId"] = value; }
+		}
+
+		#endregion
+
+		#region	Initialisation
+
+		/// <summary>
+		/// Declares the application workload type
+		/// when connecting to a database in an SQL Server Availability Group.
+		/// Default value is <c>ReadWrite</c>.
+		/// </summary>
+		/// <remarks>Possible value is either <c>ReadWrite</c> or <c>ReadOnly</c>.</remarks>
+		[ConfigurationProperty("applicationIntent", DefaultValue = "ReadWrite", IsRequired = false)]
+		public string ApplicationIntent
+		{
+			get { return (string) this["applicationIntent"]; }
+			set { this["applicationIntent"] = value; }
+		}
+
+		// applicationIntent
+
+		/// <summary>
 		/// Gets or sets a value that indicates whether asynchronous processing is allowed
 		/// by the connection created by using this connection string or not.
 		/// Default value is <c>False</c>.
@@ -85,18 +184,6 @@ namespace DataAccessFramework.Configuration
 		{
 			get { return (bool)this["asynchronousProcessing"]; }
 			set { this["asynchronousProcessing"] = value; }
-		}
-
-		/// <summary>
-		/// Gets or sets a string that contains the name of the primary data file.
-		/// This includes the full path name of an attachable database.
-		/// Default value is <see cref="String.Empty" />.
-		/// </summary>
-		[ConfigurationProperty("attachDbFilename", DefaultValue = "", IsRequired = false)]
-		public string AttachDbFilename
-		{
-			get { return (string)this["attachDbFilename"]; }
-			set { this["attachDbFilename"] = value; }
 		}
 
 		/// <summary>
@@ -112,18 +199,6 @@ namespace DataAccessFramework.Configuration
 		}
 
 		/// <summary>
-		/// Gets or sets a value that indicates whether a client/server or in-process connection
-		/// to SQL Server should be made.
-		/// Default value is <c>False</c>.
-		/// </summary>
-		[ConfigurationProperty("contextConnection", DefaultValue = false, IsRequired = false)]
-		public bool ContextConnection
-		{
-			get { return (bool)this["contextConnection"]; }
-			set { this["contextConnection"] = value; }
-		}
-
-		/// <summary>
 		/// Gets or sets the SQL Server Language record name.
 		/// Default value is <see cref="String.Empty" />.
 		/// </summary>
@@ -134,28 +209,9 @@ namespace DataAccessFramework.Configuration
 			set { this["currentLanguage"] = value; }
 		}
 
-		/// <summary>
-		/// Gets or sets the name or network address of the instance of SQL Server to connect to.
-		/// Default value is <see cref="String.Empty" />.
-		/// </summary>
-		[ConfigurationProperty("dataSource", DefaultValue = "", IsRequired = false)]
-		public string DataSource
-		{
-			get { return (string)this["dataSource"]; }
-			set { this["dataSource"] = value; }
-		}
+		#endregion
 
-		/// <summary>
-		/// Gets or sets a value that indicates whether SQL Server uses SSL encryption
-		/// for all data sent between the client and server if the server has a certificate installed.
-		/// Default value is <c>False</c>.
-		/// </summary>
-		[ConfigurationProperty("encrypt", DefaultValue = false, IsRequired = false)]
-		public bool Encrypt
-		{
-			get { return (bool)this["encrypt"]; }
-			set { this["encrypt"] = value; }
-		}
+		#region	Pooling
 
 		/// <summary>
 		/// Gets or sets a value that indicates whether the SQL Server connection pooler
@@ -167,42 +223,6 @@ namespace DataAccessFramework.Configuration
 		{
 			get { return (bool)this["enlist"]; }
 			set { this["enlist"] = value; }
-		}
-
-		/// <summary>
-		/// Gets or sets the name or address of the partner server to connect to
-		/// if the primary server is down.
-		/// Default value is <see cref="String.Empty" />.
-		/// </summary>
-		[ConfigurationProperty("failoverPartner", DefaultValue = "", IsRequired = false)]
-		public string FailoverPartner
-		{
-			get { return (string)this["failoverPartner"]; }
-			set { this["failoverPartner"] = value; }
-		}
-
-		/// <summary>
-		/// Gets or sets the name of the database associated with the connection.
-		/// Default value is <see cref="String.Empty" />.
-		/// </summary>
-		[ConfigurationProperty("initialCatalog", DefaultValue = "", IsRequired = false)]
-		public string InitialCatalog
-		{
-			get { return (string)this["initialCatalog"]; }
-			set { this["initialCatalog"] = value; }
-		}
-
-		/// <summary>
-		/// Gets or sets a value that indicates whether User ID and Password are specified
-		/// in the connection (when false) or whether the current Windows account credentials
-		/// are used for authentication (when true).
-		/// Default value is <c>False</c>.
-		/// </summary>
-		[ConfigurationProperty("integratedSecurity", DefaultValue = false, IsRequired = false)]
-		public bool IntegratedSecurity
-		{
-			get { return (bool)this["integratedSecurity"]; }
-			set { this["integratedSecurity"] = value; }
 		}
 
 		/// <summary>
@@ -242,38 +262,59 @@ namespace DataAccessFramework.Configuration
 		}
 
 		/// <summary>
-		/// Gets or sets a value that indicates whether multiple active result sets
-		/// can be associated with the associated connection.
+		/// Gets or sets a value that indicates whether the connection will be pooled
+		/// or explicitly opened every time that the connection is requested.
+		/// Default value is <c>True</c>.
+		/// </summary>
+		[ConfigurationProperty("pooling", DefaultValue = true, IsRequired = false)]
+		public bool Pooling
+		{
+			get { return (bool)this["pooling"]; }
+			set { this["pooling"] = value; }
+		}
+
+		#endregion
+
+		#region	Replication
+
+		/// <summary>
+		/// Gets or sets a value that indicates whether replication is supported using the connection.
 		/// Default value is <c>False</c>.
 		/// </summary>
-		[ConfigurationProperty("multipleActiveResultSets", DefaultValue = false, IsRequired = false)]
-		public bool MultipleActiveResultSets
+		[ConfigurationProperty("replication", DefaultValue = false, IsRequired = false)]
+		public bool Replication
 		{
-			get { return (bool)this["multipleActiveResultSets"]; }
-			set { this["multipleActiveResultSets"] = value; }
+			get { return (bool)this["replication"]; }
+			set { this["replication"] = value; }
+		}
+
+		#endregion
+
+		#region	Security
+
+		/// <summary>
+		/// Gets or sets a value that indicates whether SQL Server uses SSL encryption
+		/// for all data sent between the client and server if the server has a certificate installed.
+		/// Default value is <c>False</c>.
+		/// </summary>
+		[ConfigurationProperty("encrypt", DefaultValue = false, IsRequired = false)]
+		public bool Encrypt
+		{
+			get { return (bool)this["encrypt"]; }
+			set { this["encrypt"] = value; }
 		}
 
 		/// <summary>
-		/// Gets or sets a string that contains the name of the network library
-		/// used to establish a connection to the SQL Server.
-		/// Default value is <see cref="String.Empty" />.
+		/// Gets or sets a value that indicates whether User ID and Password are specified
+		/// in the connection (when false) or whether the current Windows account credentials
+		/// are used for authentication (when true).
+		/// Default value is <c>False</c>.
 		/// </summary>
-		[ConfigurationProperty("networkLibrary", DefaultValue = "", IsRequired = false)]
-		public string NetworkLibrary
+		[ConfigurationProperty("integratedSecurity", DefaultValue = false, IsRequired = false)]
+		public bool IntegratedSecurity
 		{
-			get { return (string)this["networkLibrary"]; }
-			set { this["networkLibrary"] = value; }
-		}
-
-		/// <summary>
-		/// Gets or sets the size in bytes of the network packets used to communicate with an instance of SQL Server.
-		/// Default value is <c>8000</c>.
-		/// </summary>
-		[ConfigurationProperty("packetSize", DefaultValue = 8000, IsRequired = false)]
-		public int PacketSize
-		{
-			get { return (int)this["packetSize"]; }
-			set { this["packetSize"] = value; }
+			get { return (bool)this["integratedSecurity"]; }
+			set { this["integratedSecurity"] = value; }
 		}
 
 		/// <summary>
@@ -302,41 +343,6 @@ namespace DataAccessFramework.Configuration
 		}
 
 		/// <summary>
-		/// Gets or sets a value that indicates whether the connection will be pooled
-		/// or explicitly opened every time that the connection is requested.
-		/// Default value is <c>True</c>.
-		/// </summary>
-		[ConfigurationProperty("pooling", DefaultValue = true, IsRequired = false)]
-		public bool Pooling
-		{
-			get { return (bool)this["pooling"]; }
-			set { this["pooling"] = value; }
-		}
-
-		/// <summary>
-		/// Gets or sets a value that indicates whether replication is supported using the connection.
-		/// Default value is <c>False</c>.
-		/// </summary>
-		[ConfigurationProperty("replication", DefaultValue = false, IsRequired = false)]
-		public bool Replication
-		{
-			get { return (bool)this["replication"]; }
-			set { this["replication"] = value; }
-		}
-
-		/// <summary>
-		/// Gets or sets a string value that indicates how the connection maintains its
-		/// association with an enlisted <see cref="System.Transactions"/> transaction.
-		/// Default value is <see cref="String.Empty" />.
-		/// </summary>
-		[ConfigurationProperty("transactionBinding", DefaultValue = "", IsRequired = false)]
-		public string TransactionBinding
-		{
-			get { return (string)this["transactionBinding"]; }
-			set { this["transactionBinding"] = value; }
-		}
-
-		/// <summary>
 		/// Gets or sets a value that indicates whether the channel will be encrypted
 		/// while bypassing walking the certificate chain to validate trust.
 		/// Default value is <c>False</c>.
@@ -349,17 +355,6 @@ namespace DataAccessFramework.Configuration
 		}
 
 		/// <summary>
-		/// Gets or sets a string value that indicates the type system the application expects.
-		/// Default value is <see cref="String.Empty" />.
-		/// </summary>
-		[ConfigurationProperty("typeSystemVersion", DefaultValue = "", IsRequired = false)]
-		public string TypeSystemVersion
-		{
-			get { return (string)this["typeSystemVersion"]; }
-			set { this["typeSystemVersion"] = value; }
-		}
-
-		/// <summary>
 		/// Gets or sets the user ID to be used when connecting to SQL Server.
 		/// Default value is <see cref="String.Empty" />.
 		/// </summary>
@@ -369,6 +364,80 @@ namespace DataAccessFramework.Configuration
 		{
 			get { return (string)this["userId"]; }
 			set { this["userId"] = value; }
+		}
+
+		#endregion
+
+		#region	Source
+
+		/// <summary>
+		/// Gets or sets a string that contains the name of the primary data file.
+		/// This includes the full path name of an attachable database.
+		/// Default value is <see cref="String.Empty" />.
+		/// </summary>
+		[ConfigurationProperty("attachDbFilename", DefaultValue = "", IsRequired = false)]
+		public string AttachDbFilename
+		{
+			get { return (string)this["attachDbFilename"]; }
+			set { this["attachDbFilename"] = value; }
+		}
+
+		/// <summary>
+		/// Gets or sets a value that indicates whether a client/server or in-process connection
+		/// to SQL Server should be made.
+		/// Default value is <c>False</c>.
+		/// </summary>
+		[ConfigurationProperty("contextConnection", DefaultValue = false, IsRequired = false)]
+		public bool ContextConnection
+		{
+			get { return (bool)this["contextConnection"]; }
+			set { this["contextConnection"] = value; }
+		}
+
+		/// <summary>
+		/// Gets or sets the name or network address of the instance of SQL Server to connect to.
+		/// Default value is <see cref="String.Empty" />.
+		/// </summary>
+		[ConfigurationProperty("dataSource", DefaultValue = "", IsRequired = false)]
+		public string DataSource
+		{
+			get { return (string)this["dataSource"]; }
+			set { this["dataSource"] = value; }
+		}
+
+		/// <summary>
+		/// Gets or sets the name or address of the partner server to connect to
+		/// if the primary server is down.
+		/// Default value is <see cref="String.Empty" />.
+		/// </summary>
+		[ConfigurationProperty("failoverPartner", DefaultValue = "", IsRequired = false)]
+		public string FailoverPartner
+		{
+			get { return (string)this["failoverPartner"]; }
+			set { this["failoverPartner"] = value; }
+		}
+
+		/// <summary>
+		/// Gets or sets the name of the database associated with the connection.
+		/// Default value is <see cref="String.Empty" />.
+		/// </summary>
+		[ConfigurationProperty("initialCatalog", DefaultValue = "", IsRequired = false)]
+		public string InitialCatalog
+		{
+			get { return (string)this["initialCatalog"]; }
+			set { this["initialCatalog"] = value; }
+		}
+
+		/// <summary>
+		/// Gets or sets a value that indicates whether to provide faster detection of and
+		/// connection to the (currently) active server.
+		/// Default value is <c>False</c>.
+		/// </summary>
+		[ConfigurationProperty("multiSubnetFailover", DefaultValue = false, IsRequired = false)]
+		public bool MultiSubnetFailover
+		{
+			get { return (bool)this["multiSubnetFailover"]; }
+			set { this["multiSubnetFailover"] = value; }
 		}
 
 		/// <summary>
@@ -384,16 +453,6 @@ namespace DataAccessFramework.Configuration
 			set { this["userInstance"] = value; }
 		}
 
-		/// <summary>
-		/// Gets or sets the name of the workstation connecting to SQL Server.
-		/// Default value is <see cref="String.Empty" />.
-		/// </summary>
-		/// <remarks>If <c>IntegratedSecurity</c> is set to <c>True</c>, this is not necessary.</remarks>
-		[ConfigurationProperty("workstationId", DefaultValue = "", IsRequired = false)]
-		public string WorkstationId
-		{
-			get { return (string)this["workstationId"]; }
-			set { this["workstationId"] = value; }
-		}
+		#endregion
 	}
 }
